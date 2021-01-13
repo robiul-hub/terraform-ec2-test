c7e86e4eb61493755a6e7e7a0732a1a2d2b91162
robiul-hub <75095486+robiul-hub@users.noreply.github.com>
2021-01-13 18:13:35 +0000

    Update example.tf

--- example.tf
+++ example.tf
@@ -8,18 +8,5 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 }
 
-terraform {
-+ backend "remote" {
-+   organization = "<ORG_NAME>"
-+   workspaces {
-+     name = "Example-Workspace"
-+   }
-+ }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
-    }
-  }
-}
+
 
